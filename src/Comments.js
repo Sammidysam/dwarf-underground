7f9d3f3eac4eebf181f11c1137a360001064e70a
Sam Craig <craigsamm@gmail.com>
2018-06-11 14:27:18 -0400

    Submit comments, but do not show them yet

--- src/Comments.js
+++ src/Comments.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 
 class Comments extends Component {
-    
+    constructor () {
+        super()
+
+        this.state = {
+            comments: []
+        }
+
+        this.addComment = this.addComment.bind(this)
+    }
+
+    addComment(event) {
+        const form = event.target
+        event.preventDefault()
+
+        const comments = [...this.state.comments]
+
+        comments.push(form.comment.value)
+
+        this.setState({ comments })
+
+        form.reset()
+        form.comment.focus()
+    }
 
     render () {
         if (this.props.on) {
         return (
-            <textarea></textarea>
+            <form onSubmit={this.addComment}>
+            <textarea name="comment"></textarea>
+            <button type="submit">Submit</button>
+            </form>
         )
     } else {
         return null
